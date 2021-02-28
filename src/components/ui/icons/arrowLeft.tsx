41a602e7261302798b2ee98e6e094b8bad43c238
Leandro Paixão <leandropp.ti@gmail.com>
2021-02-28 17:55:00 -0300

    icons e assets

---
+++ src/components/ui/icons/arrowLeft.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ArrowLeft: React.FC = () => (
+    <svg
+        viewBox="0 0 6 12"
+        fill='#8E8E8E'
+        width='8px'
+    >
+        <path d="m6.4925 0.69249a0.9959 0.9959 0 0 0-1.41 0l-4.59 4.59c-0.39 0.39-0.39 1.02 0 1.41l4.59 4.59c0.39 0.39 1.02 0.39 1.41 0s0.39-1.02 0-1.41l-3.88-3.89 3.88-3.88c0.39-0.39 0.38-1.03 0-1.41z"/>
+    </svg>
+);
+
+export default ArrowLeft;
---
+++ src/components/ui/icons/search.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const SearchIcon: React.FC = () => {
+  return(
+    <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="#8e8e8e"
+    >
+        <path 
+            d="M19.665,18.049,14.48,12.864a5.718,5.718,0,1,0-1.616,1.616l5.185,5.185a1.143,1.143,0,1,0,1.616-1.616ZM5.711,9.714a4,4,0,1,1,4,4A4,4,0,0,1,5.711,9.714Z"
+            transform="translate(-4 -4)"
+        />
+    </svg>
+  );
+}
+
+export default SearchIcon;
